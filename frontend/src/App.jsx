ecafae65b631cd813973fe9b4b03cd93d6b905ca
poisondarterz <jc.chuan.0303@gmail.com>
2024-04-14 18:24:22 +0800

    tailwind, topnav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,41 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import logo from './logo.svg';
 import './App.css';
+import './tailwind.css';
+
+// General module routes
+
+// Attendance module routes
+
+// Community module routes
+
+// Helpdesk module routes
+
+// Info module routes
+
+// Salary module routes
+import ViewSalary from './pages/salary/viewSalary';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* General module routes */}
+
+        {/* Attendance module routes */}
+
+        {/* Community module routes */}
+
+        {/* Helpdesk module routes */}
+
+        {/* Info module routes */}
+
+        {/* Salary module routes */}
+        <Route path="/salary/view" element={<ViewSalary />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/salary/viewSalary.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import TopNavBlack from "../../components/TopNavBlack";
+
+function ViewSalary() {
+  return (
+    <div>
+      <TopNavBlack />
+      <div className="container">
+        <h1>View Salary</h1>
+      </div>
+    </div>
+  );
+}
+
+export default ViewSalary;
