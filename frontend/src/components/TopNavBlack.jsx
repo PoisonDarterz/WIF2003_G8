ede2b30234d46bfb760290010d5546d5027d3f29
poisondarterz <jc.chuan.0303@gmail.com>
2024-04-21 23:22:57 +0800

    revert

--- frontend/src/components/TopNavBlack.jsx
+++ frontend/src/components/TopNavBlack.jsx
@@ -1,23 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { navItems } from "./navItems";
 
 function TopNavBlack() {
-  const [activeLink, setActiveLink] = useState("");
+  const [activeLink, setActiveLink] = useState('');
   const location = useLocation();
 
   useEffect(() => {
-    if (location.pathname === "/") {
-      setActiveLink("HOME");
-    } else if (location.pathname.startsWith("/attendance")) {
-      setActiveLink("ATTENDANCE");
-    } else if (location.pathname.startsWith("/salary")) {
-      setActiveLink("SALARY");
-    } else if (location.pathname === "/info") {
-      setActiveLink("INFORMATION");
-    } else if (location.pathname === "/community") {
-      setActiveLink("COMMUNITY");
-    } else if (location.pathname === "/helpdesk") {
-      setActiveLink("HELPDESK");
+    if (location.pathname === '/attendance') {
+      setActiveLink('ATTENDANCE');
+    } else if (location.pathname.startsWith('/salary')) {
+      setActiveLink('SALARY');
+    } else if (location.pathname === '/info') {
+      setActiveLink('INFORMATION');
+    } else if (location.pathname === '/community') {
+      setActiveLink('COMMUNITY');
+    } else if (location.pathname === '/helpdesk') {
+      setActiveLink('HELPDESK');
+    } else if (location.pathname === '/') {
+      setActiveLink('HOME');
     }
   }, [location]);
 
@@ -26,75 +27,23 @@ function TopNavBlack() {
       <Link to="/">
         <img src="/LogoText.png" alt="Company Logo" className="h-12" />
       </Link>
-      <div className="space-x-16">
-        <Link
-          to="/"
-          className={
-            activeLink === "HOME" ? "font-bold text-[#EB4335]" : "font-bold"
-          }
-          onClick={() => setActiveLink("HOME")}
-        >
-          HOME
-        </Link>
-        <Link
-          to="/attendance"
-          className={
-            activeLink === "ATTENDANCE"
-              ? "font-bold text-[#EB4335]"
-              : "font-bold"
-          }
-          onClick={() => setActiveLink("ATTENDANCE")}
-        >
-          ATTENDANCE
-        </Link>
-        <Link
-          to="/salary/view"
-          className={
-            activeLink === "SALARY" ? "font-bold text-[#EB4335]" : "font-bold"
-          }
-          onClick={() => setActiveLink("SALARY")}
-        >
-          SALARY
-        </Link>
-        <Link
-          to="/info"
-          className={
-            activeLink === "INFORMATION"
-              ? "font-bold text-[#EB4335]"
-              : "font-bold"
-          }
-          onClick={() => setActiveLink("INFORMATION")}
-        >
-          EMPLOYEE INFORMATION
-        </Link>
-        <Link
-          to="/community"
-          className={
-            activeLink === "COMMUNITY"
-              ? "font-bold text-[#EB4335]"
-              : "font-bold"
-          }
-          onClick={() => setActiveLink("COMMUNITY")}
-        >
-          COMMUNITY
-        </Link>
-        <Link
-          to="/helpdesk"
-          className={
-            activeLink === "HELPDESK" ? "font-bold text-[#EB4335]" : "font-bold"
-          }
-          onClick={() => setActiveLink("HELPDESK")}
-        >
-          HELPDESK
-        </Link>
+      <div className="flex space-x-16">
+        {navItems.map((item) => (
+          <div className="relative group" key={item.title}>
+            <Link to={item.link} className={activeLink === item.title.toUpperCase() ? 'font-bold text-[#EB4335]' : 'font-bold'} onClick={() => setActiveLink(item.title.toUpperCase())}>{item.title}</Link>
+            {item.submenu && (
+              <div className="absolute left-0 top-full w-48 rounded-md shadow-lg bg-black text-white z-10 hidden group-hover:block border-t-2 border-transparent">
+                {item.submenu.map((subItem) => (
+                  <Link to={subItem.link} className="block px-4 py-2 text-sm" onClick={() => setActiveLink(item.title.toUpperCase())} key={subItem.title}>{subItem.title}</Link>
+                ))}
+              </div>
+            )}
+          </div>
+        ))}
       </div>
       <div>
         <Link to="/general/Profile">
-          <img
-            src="/Profile.png"
-            alt="Profile"
-            className="h-8 cursor-pointer"
-          />
+          <img src="/Profile.png" alt="Profile" className="h-8 cursor-pointer" />
         </Link>
       </div>
     </nav>
--- frontend/src/pages/general/Profile.jsx
+++ frontend/src/pages/general/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import TopNavBlack from "../../components/TopNavBlack";
 
 const Profile = () => {
@@ -10,9 +11,9 @@ const Profile = () => {
             <div className="absolute top-0 left-0 w-full h-full bg-cover" style={{ backgroundImage: `url(${backgroundImage})` }}></div>
             
             <div className="container mx-auto h-screen flex flex-col justify-center items-center text-black relative z-10">
-                <div className="bg-opacity-80 bg-white p-8 rounded-lg w-1/4"> 
+                <div className="bg-opacity-80 bg-white p-8 rounded-lg w-1/3"> 
                     <div className="flex justify-center items-center mb-8">
-                        <img src="/Profile_image.jpg" alt="Profile" className="h-24 w-24 rounded-full border-2 border-white" />
+                        <img src="/Profile_image.jpg" alt="Profile" className="h-32 w-32 rounded-full border-2 border-white" />
                     </div>
                     <div className="text-left">
                         <div className="mb-6">
@@ -24,8 +25,10 @@ const Profile = () => {
                             <p className="font-bold text-lg">Department: <span className="font-normal text-lg">HR</span></p>
                             <p className="font-bold text-lg">Position: <span className="font-normal text-lg">Manager</span></p>
                         </div>
-                        <div className="flex justify-center mt-8"> {/* Centered Log Out button */}
-                            <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">Log Out</button>
+                        <div className="flex justify-center mt-8"> 
+                            <Link to="/general/LogIn">
+                                <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">Log Out</button>
+                            </Link>
                         </div>
                     </div>
                 </div>
@@ -35,4 +38,3 @@ const Profile = () => {
 };
 
 export default Profile;
-
