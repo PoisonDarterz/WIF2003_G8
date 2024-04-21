abf6cc4adb7c08696132c79a18dc1ef01d3be806
James Wong <jwyn0803@gmail.com>
2024-04-21 23:09:02 +0800

    Attendance for Employee done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import logo from './logo.svg';
-import './App.css';
-import './tailwind.css';
+import "./App.css";
+import "./tailwind.css";
 
 // General module routes
-import Home from './pages/general/Home';
-import Profile from './pages/general/Profile';
-import LogIn from './pages/general/LogIn';
-import ForgotPassword from './pages/general/ForgotPassword';
-import SignUp from './pages/general/SignUp';
-import ResetPassword from './pages/general/ResetPassword';
+import Home from "./pages/general/Home";
+import Profile from "./pages/general/Profile";
 
 // Attendance module routes
 
@@ -22,25 +17,20 @@ import ResetPassword from './pages/general/ResetPassword';
 // Info module routes
 
 // Salary module routes
-import ViewSalary from './pages/salary/viewSalary';
-import AdminSalary from './pages/salary/AdminSalary';
+import ViewSalary from "./pages/salary/viewSalary";
+import Attendance from "./pages/attendance/Attendance";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        
         <Routes>
           {/* General module routes */}
           <Route path="/" element={<Home />} />
           <Route path="/general/Profile" element={<Profile />} />
-          <Route path="/general/LogIn" element={<LogIn />} />
-          <Route path="/general/ForgotPassword" element={<ForgotPassword />} />
-          <Route path="/general/SignUp" element={<SignUp />} />
-          <Route path="/general/ResetPassword" element={<ResetPassword />} />
 
-          
           {/* Attendance module routes */}
+          <Route path="/attendance/*" element={<Attendance />} />
 
           {/* Community module routes */}
 
@@ -50,7 +40,6 @@ function App() {
 
           {/* Salary module routes */}
           <Route path="/salary/view" element={<ViewSalary />} />
-          <Route path="/salary/adminview" element={<AdminSalary />} />
         </Routes>
       </div>
     </Router>
