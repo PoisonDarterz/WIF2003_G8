bcad1e0a4f8d6c7d602cbe5a98af1c1116cc828a
poisondarterz <jc.chuan.0303@gmail.com>
2024-05-28 01:32:44 +0800

    working mongo

--- frontend/src/components/salary/EmpNameBox.jsx
+++ frontend/src/components/salary/EmpNameBox.jsx
@@ -6,7 +6,7 @@ function EmpNameBox({employees, setEmployees, searchId, searchJobTitle, singleSe
     const fetchEmployees = async () => {
       try {
         const response = await axios.get('http://localhost:5000/api/employees'); 
-        setEmployees(response.data.map(employee => ({ id: employee.id, name: employee.name, roleId: employee.roleId  })));
+        setEmployees(response.data.map(employee => ({ id: employee.id, name: employee.name, roleId: employee.roleId, _id: employee._id  })));
       } catch (error) {
         console.error('Error fetching employees:', error);
       }
