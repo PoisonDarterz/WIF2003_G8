b33edaf55f212c4ddacabdd20cb2ef2819b86787
Ing Zhen <ingzhen2003@gmail.com>
2024-06-02 20:26:49 +0800

    add create post

--- frontend/src/pages/community/CommunityHomePage.jsx
+++ frontend/src/pages/community/CommunityHomePage.jsx
@@ -5,6 +5,7 @@ import CommunityCard from '../../components/community/CommunityCard';
 import CommunityPost from '../../components/community/CommunityPost';
 import CommunitySidebar from '../../components/community/CommunitySideBar';
 import CreatePost from '../../components/community/CreatePost';
+import axios from 'axios';
 
 const CommunityHomePage = () => {
     const [posts, setPosts] = useState([
